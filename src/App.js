2502d4e568cd46d26057279e119839083189e638
artem <artemdovger260490@mail.ru>
2023-08-21 11:47:49 +0300

    add navigation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import './App.css';
-import {CurrencyRates} from "./CurrencyRates";
-import {CurrencyConverter} from "./CurrencyConverter";
+import React from 'react';
+import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
+import {CurrencyConverter} from "./CurrencyConverter/CurrencyConverter";
+import {CurrencyRates} from "./CurrencyRates/CurrencyRates";
 
 const App = () => {
     const currencies = ['USD', 'EUR', 'GBP', 'JPY', 'CAD', 'AUD', 'CHF', 'CNY', 'SEK', 'NZD'];
 
     return (
-        <div>
-            <CurrencyConverter currencies={currencies}/>
-            <CurrencyRates currencies={currencies}/>
-        </div>
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Конвертер</Link>
+                        </li>
+                        <li>
+                            <Link to="/rates">Курсы валют</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Routes>
+                    <Route path="/" element={<CurrencyConverter currencies={currencies}/>}/>
+                    <Route path="/rates" element={<CurrencyRates currencies={currencies}/>}/>
+                </Routes>
+            </div>
+        </Router>
     );
 };
 
