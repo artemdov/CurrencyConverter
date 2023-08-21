c9ee6d9ada1ff8bc4fada454a934cb47cc530b8c
artem <artemdovger260490@mail.ru>
2023-08-21 11:38:15 +0300

    fixees

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import {CurrencyRates} from "./CurrencyRates";
+import {CurrencyConverter} from "./CurrencyConverter";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const currencies = ['USD', 'EUR', 'GBP', 'JPY', 'CAD', 'AUD', 'CHF', 'CNY', 'SEK', 'NZD'];
+
+    return (
+        <div>
+            <CurrencyConverter currencies={currencies}/>
+            <CurrencyRates currencies={currencies}/>
+        </div>
+    );
+};
 
 export default App;
